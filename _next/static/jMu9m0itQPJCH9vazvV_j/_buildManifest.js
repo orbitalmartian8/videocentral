b27e697dac0fd5b1e7800623a2490d1ed534616d
LinuxGamer <LinuxGamer@users.noreply.github.com>
2023-01-01 17:47:43 +0000

    deploy: 6bfceb271e4a876ba518880ee03df0e5b2012267

---
+++ _next/static/jMu9m0itQPJCH9vazvV_j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-5b357e94abc89b4b.js","static/css/f465b078f320d11b.css","static/chunks/pages/index-ac64022e30a1ebcc.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
