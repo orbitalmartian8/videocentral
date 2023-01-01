01348a973dae7d5d73227537acb083633044384d
Linux Gamer <charl.cj.monke@gmail.com>
2023-01-01 17:42:00 +0000

    Fix Hosting

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,103 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 2648:
+/***/ ((__unused_webpack_module, exports) => {
+
+var __webpack_unused_export__;
+
+__webpack_unused_export__ = ({
+    value: true
+});
+Object.defineProperty(exports, "Z", ({
+    enumerable: true,
+    get: function() {
+        return _interopRequireDefault;
+    }
+}));
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+
+
+/***/ }),
+
+/***/ 4297:
+/***/ ((module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({
+    value: true
+}));
+exports["default"] = void 0;
+var _async_to_generator = (__webpack_require__(932)/* ["default"] */ .Z);
+var _interop_require_default = (__webpack_require__(2648)/* ["default"] */ .Z);
+var _react = _interop_require_default(__webpack_require__(6689));
+var _utils = __webpack_require__(9232);
+function appGetInitialProps(_) {
+    return _appGetInitialProps.apply(this, arguments);
+}
+function _appGetInitialProps() {
+    _appGetInitialProps = /**
+ * `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
+ * This allows for keeping state between navigation, custom error handling, injecting additional data.
+ */ _async_to_generator(function*({ Component , ctx  }) {
+        const pageProps = yield (0, _utils).loadGetInitialProps(Component, ctx);
+        return {
+            pageProps
+        };
+    });
+    return _appGetInitialProps.apply(this, arguments);
+}
+var _Component;
+class App extends (_Component = _react.default.Component) {
+    render() {
+        const { Component , pageProps  } = this.props;
+        return /*#__PURE__*/ _react.default.createElement(Component, Object.assign({}, pageProps));
+    }
+}
+App.origGetInitialProps = appGetInitialProps;
+App.getInitialProps = appGetInitialProps;
+exports["default"] = App;
+if ((typeof exports.default === "function" || typeof exports.default === "object" && exports.default !== null) && typeof exports.default.__esModule === "undefined") {
+    Object.defineProperty(exports.default, "__esModule", {
+        value: true
+    });
+    Object.assign(exports.default, exports);
+    module.exports = exports.default;
+} //# sourceMappingURL=_app.js.map
+
+
+/***/ }),
+
+/***/ 9232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [932], () => (__webpack_exec__(4297)));
+module.exports = __webpack_exports__;
+
+})();
