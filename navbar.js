55be843db1ad1044233f5eb9958c5c58d2949895
Linux Gamer <charl.cj.monke@gmail.com>
2023-01-06 19:03:01 +0000

    Back to Daisyui

--- navbar.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector('.menu-button');
-const navMenu = document.querySelector('header ul');
-
-menuButton.addEventListener('click', function() {
-  navMenu.classList.toggle('show');
-});
