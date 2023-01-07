8b2508d0ee0bcc9c19d5a1b9085a0559eaa7325d
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-01-07 22:06:35 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,29 @@ export default function Home() {
         </p>
         <video class="video-js vjs-theme-city" src="Archive/users/Project/videos/videofiles/videorendertest1.mp4"></video>
 
+  <video
+    id="my-video"
+    class="video-js"
+    controls
+    preload="auto"
+    width="640"
+    height="264"
+    poster="MY_VIDEO_POSTER.jpg"
+    data-setup="{}"
+  >
+    <source src="Archive/users/Project/videos/videofiles/videorendertest1.mp4" type="video/mp4" />
+    <source src="Archive/users/Project/videos/videofiles/videorendertest1.webm" type="video/webm" />
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a
+      web browser that
+      <a href="https://videojs.com/html5-video-support/" target="_blank"
+        >supports HTML5 video</a
+      >
+    </p>
+  </video>
+
+  <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
+
       </main>
 
       <footer>
