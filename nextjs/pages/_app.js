9c2f9775f274e52c617974c49c38876585fb7bb0
Linux Gamer <charl.cj.monke@gmail.com>
2023-01-07 18:05:13 +0000

    Update (for help)

---
+++ nextjs/pages/_app.js
@@ -0,0 +1,6 @@
+// pages/_app.js
+import '../styles/navbar.css'
+
+export default function VideoCentral({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ nextjs/pages/index.js
@@ -0,0 +1,88 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar.js';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>VideoCentral</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://github.com/LinuxGamer/videocentral" target="_blank" rel="noreferrer">VideoCentral</a>
+        </h1>
+
+        <p className={styles.description}>
+          A FOSS video hosting platform.
+        </p>
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
